50f2d3ebfaad18adf0e351fd1c4e83bb27b48158
kkvaldys <vladbigun677@gmail.com>
2024-05-11 20:34:34 +0300

    completed

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,16 @@
+import Contact from "../Contact/Contact";
+
+export default function ContactList({ contacts, onDeleteContact }) {
+  return (
+    <ul className="contact-list">
+      {contacts.map((contact) => (
+        <Contact
+          id={contact.id}
+          name={contact.name}
+          number={contact.number}
+          deleteId={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/Searchbox/Searchbox.jsx
@@ -0,0 +1,16 @@
+export default function Searchbox({ searchQuery, setSearchQuery }) {
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
